ae086ae32a24f2ccea07906bf67541f7b2c441a3
rooney123777 <rooney123777@gmail.com>
2021-01-25 21:38:23 +0200

    Double utils round method.

--- src/main/java/com/opasnite/java/commons/numeric/DoubleUtils.java
+++ src/main/java/com/opasnite/java/commons/numeric/DoubleUtils.java
@@ -1,5 +1,7 @@
 package com.opasnite.java.commons.numeric;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 
@@ -26,4 +28,12 @@ public class DoubleUtils {
         DecimalFormat groupeddecimalFormat = new DecimalFormat(DEFAULT_FORMAT, symbols);
         return groupeddecimalFormat.format(number);
     }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
 }
